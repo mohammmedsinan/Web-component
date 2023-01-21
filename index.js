5f0d00f13efb9875e536191b65f07add8eb94e61
mohammmedsinan <75750652+mohammmedsinan@users.noreply.github.com>
2023-01-21 21:37:01 +0300

    shadow root

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+label{
+    color:red;
+    font-size:50px;
+}
+input {
+    padding: 70px;
+    width: 60px;
+    height: 30px;
+}
+</style>
+<input type="checkbox" label=""/> 
+<label>
+<slot></slot>
+</label>
+`;
+
 class customElement extends HTMLElement {
   constructor() {
     super();
-    this.innerHTML = 'Hello World !';
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.append(template.content.cloneNode(true));
   }
 }
 
